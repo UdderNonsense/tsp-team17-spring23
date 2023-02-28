98093bda368edaec131e1f30a58d8c6a816f01f4
Tyler Birnie <tbirnie@superiortech.io>
2023-02-28 00:55:26 -0500

    uploading first draft of frontend. includes static html/css/js

---
+++ java-full-stack/frontend/src/router/routes.js
@@ -0,0 +1,29 @@
+// import { createRouter, createWebHistory } from 'vue-router'
+// import HomeView from '../views/HomeView.vue'
+// import AboutView from '../views/AboutView.vue'
+// import KanbanView from '../views/KanbanView.vue'
+
+// const routes = [
+//   {
+//     path: '/',
+//     name: 'Home',
+//     component: HomeView
+//   },
+//   {
+//     path: '/about',
+//     name: 'About',
+//     component: AboutView
+//   },
+//   {
+//     path: '/kanban',
+//     name: 'Kanban',
+//     component: KanbanView
+//   }
+// ]
+
+// const router = createRouter({
+//   history: createWebHistory(process.env.BASE_URL),
+//   routes
+// })
+
+// export default router
