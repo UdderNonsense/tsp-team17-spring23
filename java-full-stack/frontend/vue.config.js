1b57755eeb2faeac6540034c619997d53efc9df1
Tyler Birnie <tbirnie@superiortech.io>
2023-04-21 20:55:09 -0400

    Cleaning up the java-full-stack directory. The frontend/ inside will now display the generic Vue app when launched.

--- java-full-stack/frontend/vue.config.js
+++ java-full-stack/frontend/vue.config.js
@@ -2,21 +2,3 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
-module.exports = {
-  publicPath: '/'
-}
-
-// vue.config.js
-module.exports = {
-  // https://cli.vuejs.org/config/#devserver-proxy
-  devServer: {
-    port: 3000,
-    proxy: {
-      "/api": {
-        target: "http://localhost:8080",
-        ws: true,
-        changeOrigin: true,
-      },
-    },
-  },
-};
